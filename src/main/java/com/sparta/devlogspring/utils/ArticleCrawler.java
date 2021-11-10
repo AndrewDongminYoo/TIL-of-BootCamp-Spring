bddedf3b50f55408bd5edb2d20c30f769e47cc70
AndrewDongminYoo <ydm2790@gmail.com>
2021-11-10 23:38:47 +0900

    host name detect scrap::feat+

--- src/main/java/com/sparta/devlogspring/utils/ArticleCrawler.java
+++ src/main/java/com/sparta/devlogspring/utils/ArticleCrawler.java
@@ -7,19 +7,57 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.stereotype.Component;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 @Component
 public class ArticleCrawler {
-    static ChromeOptions options = new ChromeOptions()
-            .addArguments("--headless", "--no-sandbox", "--single-process", "--disable-dev-shm-usage");
-    static ChromeDriver driver = new ChromeDriver(options);
+
+    public static ChromeDriver getStarted() {
+        ChromeOptions options = new ChromeOptions()
+                .addArguments("--headless", "--no-sandbox", "--single-process", "--disable-dev-shm-usage");
+        return new ChromeDriver(options);
+    }
+
+    public static void finish(ChromeDriver chromeDriver) {
+        chromeDriver.quit();
+    }
+
+    public static String extractHost(String urlString) {
+        String result = "";
+        try {
+            URL url = new URL(urlString);
+            String host = url.getHost();
+            Pattern pattern = Pattern.compile("(?:blog\\.)*[a-z]+\\.[a-z]{2,4}$");
+            Matcher match = pattern.matcher(host);
+            if (match.find()) {
+                result =  match.group();
+            }
+        } catch (MalformedURLException e) {
+            result = e.getMessage();
+        }
+        return result;
+    }
+
+    public static ArrayList<String> crawlerRouter(String url) {
+        if (Objects.equals(extractHost(url),"tistory.com")) {
+            return sitemapCrawl(url);
+        } else if (Objects.equals(extractHost(url),"github.io")) {
+            return sitemapCrawl(url);
+        } else if (Objects.equals(extractHost(url),"velog.io")) {
+            return velocityCrawl(url);
+        }
+        return new ArrayList<>();
+    }
 
     public static ArrayList<String> sitemapCrawl(String url) {
+        ChromeDriver driver = getStarted();
         driver.get(url + "sitemap");
         String source = driver.getPageSource();
         ArrayList<String> arrays = new ArrayList<>();
@@ -31,23 +69,24 @@ public class ArticleCrawler {
             Matcher matcher = Pattern.compile(pattern).matcher(source);
             while (matcher.find()) arrays.add(matcher.group());
         }
+        finish(driver);
         return arrays;
     }
 
     public static ArrayList<String> velocityCrawl(String url) {
+        ChromeDriver driver = getStarted();
         driver.get(url);
-        JavascriptExecutor jsExecutor = driver;
         String scroll = "window.scrollTo(0, document.body.scrollHeight);";
         String height = "return document.body.scrollHeight;";
-        Long lastHeight = (Long) jsExecutor.executeScript(height);
+        Long lastHeight = (Long) ((JavascriptExecutor) driver).executeScript(height);
         while (true) {
-            jsExecutor.executeScript(scroll);
+            ((JavascriptExecutor) driver).executeScript(scroll);
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            Long newHeight = (Long) jsExecutor.executeScript(height);
+            Long newHeight = (Long) ((JavascriptExecutor) driver).executeScript(height);
             if (lastHeight.equals(newHeight)) break;
             lastHeight = newHeight;
         }
@@ -57,12 +96,7 @@ public class ArticleCrawler {
         for (WebElement element : elementList) {
             resultArray.add(element.getAttribute("href"));
         }
+        finish(driver);
         return resultArray;
-    }
-
-    public static void main(String[] args) {
-        ArrayList<String> urlList = ArticleCrawler.sitemapCrawl("https://cat-minzzi.tistory.com/");
-        ArrayList<String> urlList2 = ArticleCrawler.velocityCrawl("https://velog.io/@yu_jep/");
-        System.out.println(urlList2);
     }
 }
